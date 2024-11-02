6f578b7692eaf4a76dba92c4941780e1d5ad5c32
anand <anand@anands-MacBook-Air.local>
2024-11-02 04:53:41 -0400

    commit5

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, 'views'));
+
 
 app.get('/', (req, res) => {
   res.render('home');
